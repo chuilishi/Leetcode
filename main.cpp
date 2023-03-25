d620bb5f31442a5ddb2c87a8b41f6ce8c5f984a8
chuilishi <1483843127@qq.com>
2023-03-25 18:36:17 +0800

    v1

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "iostream"
+using namespace::std;
+int num[10];
+int dp[10];
+int main(){
+    for(int i=0;i<8;i++){
+        cin>>num[i];
+        dp[i]=1;
+    }
+    for(int i=1;i<8;i++){
+        
+    }
+}

2ea1e778705f1b7dc66f340ae855ca7c7e340c53
chuilishi <1483843127@qq.com>
2023-03-26 23:27:59 +0800

    解决(没完全)版

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ public:
         for(int i=1;i<len;i++){
             if(s[i]==')'){
                 if(s[i-1]=='('){
-                    dp[i]=dp[i-2]+2;
+                    dp[i]=(i>=2?dp[i-2]:0)+2;
                 }else if(i-1-dp[i-1]>=0&&dp[i-1-dp[i-1]]=='('){
                     dp[i]=dp[i-1]+2+(i-2-dp[i-1]>=0?dp[i-2-dp[i-1]]:0);
                 }

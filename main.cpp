f8b839ceefec6c90068c7cb1ca135b29d7beb14e
chuilishi <1483843127@qq.com>
2023-03-26 23:47:11 +0800

    过了

--- main.cpp
+++ main.cpp
@@ -8,12 +8,11 @@ public:
         if(s.length()==0)return 0;
         int len=s.length();
         vector<int>dp(len,0);
-        dp[0]=0;
         for(int i=1;i<len;i++){
             if(s[i]==')'){
                 if(s[i-1]=='('){
                     dp[i]=(i>=2?dp[i-2]:0)+2;
-                }else if(i-1-dp[i-1]>=0&&dp[i-1-dp[i-1]]=='('){
+                }else if((i-1-dp[i-1]>=0)&&(s[i-1-dp[i-1]]=='(')){
                     dp[i]=dp[i-1]+2+(i-2-dp[i-1]>=0?dp[i-2-dp[i-1]]:0);
                 }
             }
@@ -23,5 +22,5 @@ public:
 };
 int main(){
     Solution s;
-    cout<<s.longestValidParentheses("()");
+    cout<<s.longestValidParentheses("()(())");
 }
